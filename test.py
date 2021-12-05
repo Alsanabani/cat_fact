fc8e689ec968ed6061ac27df8a7fc3b6571b5ca0
Alsanabani <47475821+Alsanabani@users.noreply.github.com>
2021-12-05 15:29:04 +0000

    second commit

---
+++ test.py
@@ -0,0 +1,13 @@
+print("hello there")
+# %%
+d = 2 + 4
+print(d)
+# %%
+print("hello there 2")
+# %%
+my_first_var = 2 + 2
+my_first_var
+# %%
+my_first_var = my_first_var + 4
+print(my_first_var)
+# %%
